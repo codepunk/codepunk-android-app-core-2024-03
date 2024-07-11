96fc71cab6f68f69f8718cb0ecfce373523ea1e2
codepunk <codepunk@codepunk.com>
2024-07-11 15:31:44 -0400

    Updated dependency versions

--- app/src/main/java/com/codepunk/skeleton/MainActivity.kt
+++ app/src/main/java/com/codepunk/skeleton/MainActivity.kt
@@ -70,11 +70,11 @@ class MainActivity : ComponentActivity() {
     private fun testStuff() {
         //testFetchArtist(TAYLOR_SWIFT)
         //testFetchLabel(REPUBLIC_RECORDS)
-        testFetchLabel(ATLANTIC_RECORDS)
+        //testFetchLabel(ATLANTIC_RECORDS)
         //testFetchMaster(AN_HOUR_BEFORE_ITS_DARK_MASTER)
         //testFetchMaster(THE_TORTURED_POETS_DEPARTMENT_MASTER)
         //testFetchMaster(THE_TORTURED_POETS_DEPARTMENT_MASTER)
-        //testFetchRelease(AN_HOUR_BEFORE_ITS_DARK_RELEASE)
+        testFetchRelease(AN_HOUR_BEFORE_ITS_DARK_RELEASE)
         //testFetchRelease(THE_TORTURED_POSTS_DEPARTMENT_THE_ANTHOLOGY)
     }
 

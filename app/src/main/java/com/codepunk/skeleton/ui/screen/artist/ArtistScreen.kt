03078a207eae62cac7b25ce1304c545c7bde978c
codepunk <codepunk@codepunk.com>
2024-07-19 13:02:30 -0400

    Cleanup

--- app/src/main/java/com/codepunk/skeleton/ui/screen/artist/ArtistScreen.kt
+++ app/src/main/java/com/codepunk/skeleton/ui/screen/artist/ArtistScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
